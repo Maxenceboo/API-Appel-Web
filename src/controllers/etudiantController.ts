bde2634219c79a7fc3bba0b1e9f046708d9f50b1
D3sTryk3 <maxenceboo@gmail.com>
2024-03-25 00:07:19 +0100

    Ajouter la fonctionnalité de création d'étudiant

--- src/controllers/etudiantController.ts
+++ src/controllers/etudiantController.ts
@@ -12,3 +12,57 @@ export const getEtudiant = async (req: Request, res: Response) => {
         res.status(500).json({ message: (error as Error).message });
     }
 }
+
+export const createEtudiant = async (req: Request, res: Response) => {
+    // let response = await axios.post('/etudiant/create', {
+    //     code_etudiant: code_etudiant,
+    //     nom: nom,
+    //     prenom: prenom,
+    //     date_naissance: date_naissance,
+    //     sexe: sexe,
+    //     nationalite: nationalite,
+    //     adrfixe1: adrfixe1,
+    //     adrfixe2: adrfixe2,
+    //     adrfixe3: adrfixe3,
+    //     adrfixee: adrfixee,
+    //     adrfixecodepostal: adrfixecodepostal,
+    //     adrfixecommune: adrfixecommune,
+    //     adrfixetelephone: adrfixetelephone,
+    //     adrfixetelportable: adrfixetelportable,
+    //     adrfixeemail: adrfixeemail,
+    //     adrann1: adrann1,
+    //     adrann2: adrann2,
+    //     adrann3: adrann3,
+    //     adranncodepostal: adranncodepostal,
+    //     adranncommune: adranncommune,
+    //     adranntelportable: adranntelportable,
+    //     adrannemail: adrannemail,
+    //     csp_chef_famille: csp_chef_famille,
+    //     csp_autre_parent: csp_autre_parent,
+    //     email_uppa: email_uppa,
+    //     code_bac_ou_eq: code_bac_ou_eq,
+    //     lib_bac_ou_eq: lib_bac_ou_eq,
+    //     annee_bac_ou_eq: annee_bac_ou_eq,
+    //     lib_mention: lib_mention,
+    //     regime_ins: regime_ins,
+    //     libelle_bourse: libelle_bourse,
+    //     lib_type_dernier_diplome: lib_type_dernier_diplome,
+    //     annee_dernier_diplome: annee_dernier_diplome,
+    //     code_composante: code_composante,
+    //     libcomposante: libcomposante,
+    //     libetape: libetape,
+    //     codeetape: codeetape,
+    //     versionetape: versionetape,
+    //     statut: statut,
+    //     ine: ine
+    // });
+    const { nom, prenom, code_etudiant, date_naissance, sexe, nationalite, adrfixe1, adrfixe2, adrfixe3, adrfixee, adrfixecodepostal, adrfixecommune, adrfixetelephone, adrfixetelportable, adrfixeemail, adrann1, adrann2, adrann3, adranncodepostal, adranncommune, adranntelportable, adrannemail, csp_chef_famille, csp_autre_parent, email_uppa, code_bac_ou_eq, lib_bac_ou_eq, annee_bac_ou_eq, lib_mention, regime_ins, libelle_bourse, lib_type_dernier_diplome, annee_dernier_diplome, code_composante, libcomposante, libetape, codeetape, versionetape, statut, ine } = req.body;
+    try {
+        await db('etudiant').insert({ nom, prenom, code_etudiant, date_naissance, sexe, nationalite, adrfixe1, adrfixe2, adrfixe3, adrfixee, adrfixecodepostal, adrfixecommune, adrfixetelephone, adrfixetelportable, adrfixeemail, adrann1, adrann2, adrann3, adranncodepostal, adranncommune, adranntelportable, adrannemail, csp_chef_famille, csp_autre_parent, email_uppa, code_bac_ou_eq, lib_bac_ou_eq, annee_bac_ou_eq, lib_mention, regime_ins, libelle_bourse, lib_type_dernier_diplome, annee_dernier_diplome, code_composante, libcomposante, libetape, codeetape, versionetape, statut, ine });
+        res.status(201).json({ message: 'Etudiant créé' });
+    } catch (error) {
+        res.status(500).json({ message: (error as Error).message });
+    }
+}
+
+
--- src/routes/etudiantRoutes.ts
+++ src/routes/etudiantRoutes.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 import * as etudiantController from '../controllers/etudiantController';
 
 router.get('/:nom/:prenom', etudiantController.getEtudiant); 
+router.get('/create', etudiantController.createEtudiant);
 
 export default router;
